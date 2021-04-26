1fc6b3d9c203cc7252f66df5a4bdd4d381c5d568
tech-hack-rich <ricardoathleta@icloud.com>
2021-04-26 18:27:47 +0900

    informationテーブルの作成

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,2 +1,9 @@
 class Information < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :name_spell, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+    validates :birthdate
+    validates :email
+    validates :motivation
+  end
 end
--- db/migrate/20210426074729_create_information.rb
+++ db/migrate/20210426074729_create_information.rb
@@ -1,7 +1,11 @@
 class CreateInformation < ActiveRecord::Migration[6.0]
   def change
     create_table :information do |t|
-
+      t.string     :name, null:false
+      t.string     :name_spell, null: false
+      t.date       :birthdate, null:false
+      t.string     :email, null:false
+      t.text       :motivation, null:false
       t.timestamps
     end
   end
