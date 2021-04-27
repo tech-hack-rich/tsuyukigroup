7a55eed96d4dec895bab2ea4b45551b4365c6210
tech-hack-rich <ricardoathleta@icloud.com>
2021-04-27 16:22:52 +0900

    git コミットの修正

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -3,6 +3,23 @@ class InformationsController < ApplicationController
   end
   
   def new
+    @information = Information.new
   end
-  
+
+  def create
+    @information = Information.new(information_params)
+    if @information.save
+      InformationMailer.information_mail(@information).deliver
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+
+  def information_params
+    params.permit(:name, :name_spell, :birthdate, :email, :motivation)
+  end
+
 end
--- test/mailers/previews/information_mailer_preview.rb
+++ test/mailers/previews/information_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/information_mailer
 class InformationMailerPreview < ActionMailer::Preview
+  def inquiry
+    information = Information.new(name: "侍 太郎", name_spell: "サムライ　タロウ", birthdate:"1998/01/25", email:"test@test", motivation: "よろしくお願いします")
 
+    InformationMailer.information_mail(information)
+  end
 end
