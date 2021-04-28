f69cf37e0e74f341d440122e5e8c59de834b6665
tech-hack-rich <ricardoathleta@icloud.com>
2021-04-28 11:35:04 +0900

    記述ミスの修正

--- app/mailers/information_mailer.rb
+++ app/mailers/information_mailer.rb
@@ -1,6 +1,6 @@
 class InformationMailer < ApplicationMailer
   def information_mail(information)
     @information = information
-    mail to: 'ENV['INFORMATION_MAIL']', subject: "採用エントリー"
+    mail to: ENV['INFORMATION_MAIL'], subject: "採用エントリー"
   end
 end
