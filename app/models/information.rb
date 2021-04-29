656abd8c2b39833822b48914dd5cbaf6c695dc4a
tech-hack-rich <ricardoathleta@icloud.com>
2021-04-30 01:17:10 +0900

    バリデーションエラーの日本語化

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,7 +1,7 @@
 class Information < ApplicationRecord
   with_options presence: true do
     validates :name
-    validates :name_spell, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+    validates :name_spell, format: { with: /\A[ァ-ヶー－]+\z/, message: 'はカタカナで入力してください' }
     validates :birthdate
     validates :email
     validates :motivation
